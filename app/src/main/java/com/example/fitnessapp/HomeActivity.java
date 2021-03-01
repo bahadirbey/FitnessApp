60609385330e8c40356ae06d0b9eb3521c8e7973
bahadirbey <68642072+bahadirbey@users.noreply.github.com>
2021-03-01 10:40:56 +0300

    Insert Operation Done

--- app/src/main/java/com/example/fitnessapp/HomeActivity.java
+++ app/src/main/java/com/example/fitnessapp/HomeActivity.java
@@ -1,18 +1,28 @@
 package com.example.fitnessapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class HomeActivity extends AppCompatActivity {
 
     private Button signoutBtn;
     private FirebaseAuth mAuth;
+
+    EditText mHeight, mWeight;
+    Button savebtn;
+
+    DatabaseReference reference;
+    Member member;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +31,25 @@ public class HomeActivity extends AppCompatActivity {
 
         signoutBtn = findViewById(R.id.signout_btn);
         mAuth = FirebaseAuth.getInstance();
+
+        mHeight = findViewById(R.id.height_text);
+        mWeight = findViewById(R.id.weight_text);
+        savebtn = findViewById(R.id.save_btn);
+
+        reference = FirebaseDatabase.getInstance().getReference().child("Member");
+
+        savebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int height = Integer.parseInt(mHeight.getText().toString().trim());
+                int weight = Integer.parseInt(mWeight.getText().toString().trim());
+
+                member = new Member(height, weight);
+
+                reference.push().setValue(member);
+                Toast.makeText(HomeActivity.this,"Your information is saved successfully!", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         signoutBtn.setOnClickListener(new View.OnClickListener() {
             @Override
---
+++ app/src/main/java/com/example/fitnessapp/Member.java
@@ -0,0 +1,27 @@
+package com.example.fitnessapp;
+
+public class Member {
+    private int height;
+    private int weight;
+
+    public Member(int height, int weight){
+        this.height = height;
+        this.weight = weight;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+}
