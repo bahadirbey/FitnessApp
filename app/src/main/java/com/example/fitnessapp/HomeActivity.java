e83a809faa3082d0f97c6745a4e7ea13bd253676
bahadirbey <68642072+bahadirbey@users.noreply.github.com>
2021-03-03 14:26:42 +0300

    Drawer Added

--- app/src/main/java/com/example/fitnessapp/HomeActivity.java
+++ app/src/main/java/com/example/fitnessapp/HomeActivity.java
@@ -1,17 +1,27 @@
 package com.example.fitnessapp;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.Toolbar;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -22,7 +32,7 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     FirebaseAuth auth;
     FirebaseUser user;
@@ -30,37 +40,78 @@ public class HomeActivity extends AppCompatActivity {
     private TextView welcome_text;
     private Button readBtn;
 
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        readBtn = findViewById(R.id.read_btn);
-        welcome_text = findViewById(R.id.welcome_text);
-
         auth = FirebaseAuth.getInstance();
         user = auth.getCurrentUser();
+
+        //+readBtn = findViewById(R.id.read_btn);
+        //welcome_text = findViewById(R.id.welcome_text);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
 
         DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("users");
         String mail = InfoActivity.email.replace("@","").replace(".","");
         Toast.makeText(HomeActivity.this,mail,Toast.LENGTH_SHORT).show();
 
-        readBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                reference.addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        String mail = InfoActivity.email.replace("@","").replace(".","");
-                        String name = dataSnapshot.child(mail).child("name").getValue().toString();
-                        welcome_text.setText(name);
-                    }
+        //readBtn.setOnClickListener(new View.OnClickListener() {
+        //    @Override
+        //    public void onClick(View v) {
+        //        reference.addValueEventListener(new ValueEventListener() {
+        //            @Override
+        //            public void onDataChange(DataSnapshot dataSnapshot) {
+        //                String mail = InfoActivity.email.replace("@","").replace(".","");
+        //                String name = dataSnapshot.child(mail).child("name").getValue().toString();
+        //                welcome_text.setText(name);
+        //            }
 
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError error) {
-                    }
-                });
-            }
-        });
+        //            @Override
+        //            public void onCancelled(@NonNull DatabaseError error) {
+        //            }
+        //        });
+        //    }
+        //});
+
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+
+        navigationView.setCheckedItem(R.id.nav_home);
+
+        Menu menu = navigationView.getMenu();
+        menu.findItem(R.id.nav_logout).setVisible(false);
+    }
+
+    public void onBackPressed(){
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else{
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.nav_home:
+                break;
+            case R.id.nav_profile:
+                Intent intent = new Intent(HomeActivity.this, Exercises.class);
+                startActivity(intent);
+                break;
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
